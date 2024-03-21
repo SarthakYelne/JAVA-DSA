bf547176c05fcfdd84dee785e4e2eaf0e9164ad2
Sarthak Yelne <sarthakyelne2003@gmail.com>
2024-03-21 22:22:04 +0530

    arrays complete

---
+++ ARRAYS/Buy_Sellstocks/Buy_Sell.java
@@ -0,0 +1,24 @@
+package ARRAYS.Buy_Sellstocks;
+
+public class Buy_Sell {
+
+    public static int BuySellstocks(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int max_profit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                max_profit = Math.max(max_profit, profit);
+            } else{
+                buyPrice = prices[i];
+            }
+        }
+
+        return max_profit;
+    }
+    public static void main(String[] args) {
+        int prices[] = {7, 1, 5, 3, 6, 4};
+        System.out.println(BuySellstocks(prices));
+    }
+}
