4e0187f9c9364e8984b40475e6be74d64422b59b
Sarthak Yelne <sarthakyelne2003@gmail.com>
2024-03-24 23:02:51 +0530

    Counting Sort

---
+++ Sorting_Algorithms/Counting_Sort/Countingsort.java
@@ -0,0 +1,43 @@
+package Sorting_Algorithms.Counting_Sort;
+
+public class Countingsort {
+    public static void CountingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+
+        for(int i=0; i<arr.length; i++) {
+            largest = Math.max(largest,arr[i]);
+        }
+
+        // counts frequnecy
+        int count[] = new int [largest+1];
+        for(int i=0; i<arr.length; i++) {
+             count[arr[i]]++;
+        }
+
+        int j = 0;
+        for(int i=0; i<count.length; i++){
+            while (count[i]>0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void prints(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,4,3,1,2,4,3,7};
+
+        CountingSort(arr);
+        prints(arr);
+    }
+}
+
+// Output : 
+
+// 1 1 2 3 3 4 4 7 
