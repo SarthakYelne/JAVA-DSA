d20ec740b8c71deef8ccc47f4a594520543edf11
Sarthak Yelne <sarthakyelne2003@gmail.com>
2024-03-26 21:05:13 +0530

    added

--- Arrays_2D/Matrix/matirx.java
+++ Arrays_2D/Matrix/matirx.java
@@ -20,22 +20,22 @@ public class matirx {
         return false;
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int matrix[][] = new int[3][3];
-        int n = matrix.length, m = matrix[0].length;
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                matrix[i][j] = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int matrix[][] = new int[3][3];
+            int n = matrix.length, m = matrix[0].length;
+            for (int i=0; i<n; i++) {
+                for (int j=0; j<m; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
             }
-        }
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                System.out.print(matrix[i][j]+" "); 
+            for (int i=0; i<n; i++) {
+                for (int j=0; j<m; j++) {
+                    System.out.print(matrix[i][j]+" "); 
+                }
+                System.out.println();
             }
-            System.out.println();
+            search(matrix, 6);
         }
-        search(matrix, 6);
     }
 }
 
